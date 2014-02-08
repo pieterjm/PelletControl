c84b38e8094297711a3c1e48a033a38758cd010f
Pellet Control <pelletcontrol@raspberrypi.(none)>
2014-02-08 22:10:27 +0100

    Unknown

--- public/app.js
+++ public/app.js
@@ -1,3 +1,10 @@
+var ellookup = {
+    'operatingmode' : 'OPERATING_MODE',
+    'temperature':  'TEMPERATURE',
+    'mode': 'MODE',
+    'settemp': 'SETTEMP'
+};
+
 require([
     "dojo/_base/connect",
     "dojo/_base/window",
@@ -20,10 +27,53 @@ require([
 ], function(connect, win, domConstruct, ready, registry, ItemFileReadStore, ProgressIndicator, ListItem, parser){
     ready(function(){
 	var socket = io.connect();
-	socket.on("pelletkachel",function(msg) {
-	    document.getElementById("TEMPERATURE").innerHTML = msg['temperature'];
-	    document.getElementById("OPERATING_MODE").innerHTML = msg['operatingmode'];
-	    document.getElementById("MODE").innerHTML = msg['mode'];
+	socket.on("pelletkachel",function(msg) {	    
+	    for (var key in msg ) {
+		var el = document.getElementById(ellookup[key]);
+		if ( el ) {		    
+		    el.innerHTML = msg[key];
+		}
+	    }
 	});	
+
+	operatingmode_dialog = function(){
+	    var node = registry.byId("pickermode");
+	    var current = document.getElementById("OPERATING_MODE").innerHTML;
+	    registry.byId('dlg_operatingmode').show();
+	}
+
+	operatingmodeHide = function(dlg){
+	    registry.byId('dlg_operatingmode').hide();
+	}
+
+	setOperatingmode = function() {
+	    console.log("set mode");
+	    var node = registry.byId("pickermode");
+	    console.log(node.value);
+	    socket.emit('setmode',{operatingmode:node.value});
+	    registry.byId('dlg_operatingmode').hide();
+	}
+
+	settemp_dialog = function(){
+	    var node = registry.byId("pickertemp");
+	    var current = document.getElementById("SETTEMP").innerHTML;
+	    while ( node.value < current )
+		node.spin(1);
+	    registry.byId('dlg_thermostat').show();
+	}
+
+	setThermostat = function() {
+	    console.log("set thermostat");
+	    var node = registry.byId("pickertemp");
+	    console.log(node.value);
+	    document.getElementById("SETTEMP").innerHTML = node.value;
+	    socket.emit('settemp',{temperature:node.value});
+	    registry.byId('dlg_thermostat').hide();
+	}
+
+	thermostatHide = function(dlg){
+	    registry.byId('dlg_thermostat').hide();
+	}
+
     });
 });
--- server.js
+++ server.js
@@ -6,12 +6,21 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var posix = require('posix');
+
+// open syslog
+posix.openlog('pelletcontrol', { cons: true, ndelay: true, pid: true }, 'local0');
+posix.syslog('info', 'pelletcontrol started');
 
 
-
+var currentTemp = "--";
+var currentOMode = "--";
+var currentMode = "--";
+var currentSetTemp = "--";
 
 // queue of messages to be sent to the pelletkachel
 var queue = [];
+var lastmode = "unknown";
 
 // pas zonodig het seriele device en de baudrate aan
 var serialPort = new SerialPort("/dev/ttyUSB0",{
@@ -19,49 +28,54 @@ var serialPort = new SerialPort("/dev/ttyUSB0",{
     parser: serialport.parsers.readline("\r")
 });
 
+function log(msg) {
+    posix.syslog('info', msg);
+//    console.log(msg);
+}
 
 
 serialPort.on("open", function () {
-    console.log('open');
     serialPort.on('data', function(data) {
 	var result = undefined;
 	
-	console.log("RECV: " + data);
+	log("RECV: " + data);
 
 	if ( /^at$/.test(data) ) {
 	    serialPort.write("OK\r");
-	    console.log("SEND: OK");
+	    log("SEND: OK");
 	} else if ( /^at\+cmgf=1$/.test(data) ) {
 	    serialPort.write("OK\r");
-	    console.log("SEND: OK");
+	    log("SEND: OK");
 	} else if ( /^at\+cmgd=\d+$/.test(data) ) {
 	    serialPort.write("OK\r");
-	    console.log("SEND: OK");
+	    log("SEND: OK");
 	} else if ( /^at\+csq$/.test(data) ) {
 	    serialPort.write("+CSQ: 9,99\r");
-	    console.log("SEND: +CSQ");
+	    log("SEND: +CSQ");
 	} else if ( /^at\+cmgr=\d+$/.test(data) ) {
 	    var msg = undefined;
 	    if ( msg = queue.shift() ) {
 		serialPort.write("+CMGR: \"REC READ\",\"0123456789\",,\"01/01/01,00:00:00+32\"\r"+ msg + "i\r\rOK\r");
-		console.log("SEND: SMS " + msg);
+		log("SEND: SMS " + msg);
 	    } else {
 		serialPort.write("+CMGR: 0,,0\rOK\r");
-		console.log("SEND: +CMGR: 0");
+		log("SEND: +CMGR: 0");
 	    }
 	} else if ( result = data.match(/^oper\.mode\ ([^,]+),\ mode\ ([^,]+),\ temp\.actual\ (\d+)\ deg/) ) {
-	    console.log("Operating mode: " + result[1]);
-	    console.log("mode: " + result[2]);
-	    console.log("temperature: " + result[3]);
-
-	    // emit event
+	    currentOMode = result[1];
+	    currentMode = result[2];
+	    currentTemp = result[3];
 	    eventEmitter.emit('pelletkachel',{operatingmode: result[1], mode: result[2], temperature: result[3]});
-
+	    lastmode = result[2];	    
+	} else if (result = data.match(/^oper\.mode\ (\S+)\ set\ temp\.set\ (\d+)\ degr\ set/) ) {
+	    currentOMode = result[1];
+	    currentSetTemp = result[2];
+	    eventEmitter.emit('pelletkachel',{operatingmode: result[1], mode: lastmode, settemp: result[2]});
 	} else if ( /^at\+cmgs=\d+$/.test(data) ) {
 	    serialPort.write("+CMGS: 1\rOK\r");
-	    console.log("SEND: +CMGS OK");
+	    log("SEND: +CMGS OK");
 	} else {
-	    console.log("PROBLEM: Unexpected data: " + data);
+	    log("PROBLEM: Unexpected data: " + data);
 	}
     });
 });
@@ -70,18 +84,53 @@ eventEmitter.on('smscommand',function(data) {
     queue.push(data);    
 });
 
+// interval status readout each 30 seconds
 setInterval(function(){
-    console.log("timed event");
+    log("timed event. queuelength = " + queue.length);
     eventEmitter.emit('smscommand',"***i");    
-},15000);
+},30000);
 
-// start webserver
+// start webserver for web control
 app.use(express.static(__dirname + '/public'));
 server.listen(8080);
 
+function emitstate(socket) {
+    socket.emit('pelletkachel', {operatingmode: currentOMode,mode: currentMode,temperature: currentTemp,settemp: currentSetTemp});
+}
+
 // forward everything to the socket
 io.sockets.on('connection', function (socket) {    
+    emitstate(socket);
+    
     eventEmitter.on('pelletkachel', function(data) {
-	socket.emit('pelletkachel', data);
+	emitstate(socket);
+    });
+
+    socket.on('settemp', function(data) {
+	console.log("Settemp: " + data.temperature);
+	var temp = Number(data.temperature).toFixed(0);
+	if ( temp >= 10 && temp < 25 ) {
+	    eventEmitter.emit('smscommand',"***baheat-rt" + temp + "#");
+	    currentSetTemp = temp;
+	}
+    });
+
+    socket.on('setmode', function(data) {
+	console.log("Setmode: " + data.operatingmode);
+
+	if ( data.operatingmode.toLowerCase() == 'off' ) {
+	    eventEmitter.emit('smscommand',"***baoff#");
+	} else if ( data.operatingmode.toLowerCase() == 'auto' ) {
+	    eventEmitter.emit('smscommand',"***baauto");
+	} else if ( data.operatingmode.toLowerCase() == 'heat' ) {
+	    if ( Number(currentSetTemp) > 0 ) { 
+		eventEmitter.emit('smscommand',"***baheat-rt" + currentSetTemp + "#");
+	    } else if ( Number(currentTemp) > 0 ) { 
+		eventEmitter.emit('smscommand',"***baheat-rt" + currentTemp + "#");
+	    }
+	}
     });
 });
+
+
+
